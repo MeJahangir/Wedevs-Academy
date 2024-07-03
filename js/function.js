11230a9c621bfe4ac28a8de6624e8abed0a34340
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-03 08:32:42 +0600

    using function and object call, apply, bind

--- js/function.js
+++ js/function.js
@@ -21,3 +21,38 @@ function Person(name, age) {
 
 const person1 = new Person("Jahangir alam", 25);
 console.log(person1);
+
+//call function
+function greet(greeting) {
+    console.log(greeting +' ' + this.name);
+}
+
+const person = {
+    name: "Jahangir Alam"
+};
+
+const test = greet.call(person, "Hello"); // "Hello, Jahangir Alam"
+
+// apply function
+function greet(greeting) {
+    console.log(greeting + ", " + this.name);
+}
+
+const person2 = {
+    name: "Jahangir Alam"
+};
+
+greet.apply(person2, ["Hello"]); // "Hello, Jahangir Alam" 
+
+// using bind function
+function greet(greeting) {
+    console.log(greeting + ", " + this.name);
+}
+
+const person3 = {
+    name: "Jahangir Alam"
+};
+
+const greetPerson = greet.bind(person3);
+greetPerson("Hello"); // "Hello, Jahangir Alam"
+
